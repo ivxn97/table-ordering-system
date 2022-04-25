4e28203d59afbf183ade2ab9b7a96b52b9d7ef36
Ivan Lee <kudoboi@hotmail.com>
2022-04-25 18:45:43 +0800

    v

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname,'./public')));
 
 
 app.get('/', function(req,res){
-  res.sendFile(path.join(__dirname,'./loginPage.html'));
+  res.sendFile(path.join(__dirname,'./html/loginPage.html'));
 });
 
 /*
