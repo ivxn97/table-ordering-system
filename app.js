b8d6ec707bdd6dd1ad30dec3f9781b5e48aae089
jtmw1012 <103188536+jtmw1012@users.noreply.github.com>
2022-04-29 17:33:13 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -14,38 +14,35 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'/public')));
 app.use('/img', express.static(__dirname + '/Images'));
 
+//      WORKS WITHOUT THIS FOR NOW      // 
+// app.get('/', function(req,res){
+//   res.sendFile(path.join(__dirname,'./public/loginPage.html'));
+// });
+// app.get('/loginPage.html', function(req,res){
+//   res.sendFile(path.join(__dirname,'./public/loginPage.html'));
+// });
+// app.get('/staffPage.html', function(req,res){
+//   res.sendFile(path.join(__dirname,'./public/staffPage.html'));
+// })
+// app.get('/ownerPage.html', function(req,res){
+//   res.sendFile(path.join(__dirname,'./public/ownerPage.html'));
+// })
+// app.get('/adminPage.html', function(req,res){
+//   res.sendFile(path.join(__dirname,'./public/adminPage.html'));
+// })
+// app.get('/managerPage.html', function(req,res){
+//   res.sendFile(path.join(__dirname,'./public/managerPage.html'));
+// })
 
-app.get('/', function(req,res){
-  res.sendFile(path.join(__dirname,'./public/loginPage.html'));
-});
 
-app.get('/loginPage.html', function(req,res){
-  res.sendFile(path.join(__dirname,'./public/loginPage.html'));
-});
-
-app.get('/staffPage.html', function(req,res){
-  res.sendFile(path.join(__dirname,'./public/staffPage.html'));
-})
-
-app.get('/ownerPage.html', function(req,res){
-  res.sendFile(path.join(__dirname,'./public/ownerPage.html'));
-})
-
-app.get('/adminPage.html', function(req,res){
-  res.sendFile(path.join(__dirname,'./public/adminPage.html'));
-})
-
-app.get('/managerPage.html', function(req,res){
-  res.sendFile(path.join(__dirname,'./public/managerPage.html'));
-})
-
+//Login validation
 app.post('/UserInfo', function(req, res) {
   var username = req.body.username;
   var password = req.body.password;
   var profileType = req.body.profileType;
 
   //if (req.body.username && req.body.password) {
-    console.log('Checking username: ' + username + ' password: ' + password + ' Profile Type: ' + profileType);
+    console.log('Checking username: ' + username + ' password: ' + password + ' profile type: ' + profileType);
     var db = new sqlite3.Database('./restaurant.db');
     db.get("SELECT * FROM accounts where (username==?) AND (password==?) AND (role==?)", 
                                   [req.body.username, req.body.password, req.body.profileType], function(err,row){
@@ -93,8 +90,31 @@ app.post('/UserInfo', function(req, res) {
         err = console.log('Error: ' + err)
       }}
     );
-  }
-);
+  //}
+});
+
+
+//Admin profile creation
+app.post('/createProfile', function(req, res){
+  var fname = req.body.fname;
+  var lname = req.body.lname;
+  var username = req.body.username;
+  var password = req.body.password;
+  var profileType = req.body.profileType;
+
+  console.log('Creating account with first name: ' + fname + ' last name' + lname  + ' username: ' + username + ' password: ' + password + ' profile type: ' + profileType);
+  var db = new sqlite3.Database('./restaurant.db');
+  db.run('INSERT INTO accounts (first_name, last_name, username, password, role) VALUES(?,?,?,?,?)', [fname, lname, username, password, profileType], function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log("Successful account creation");
+    }
+  });
+
+});
+
 
 server.listen(3000,function(){ 
   console.log("Server listening on port: 3000");
