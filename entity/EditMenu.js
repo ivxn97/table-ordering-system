3eeb0027f70a1a8bfdcfba1853ded3f1e7134829
mattyjy <104262448+mattyjy@users.noreply.github.com>
2022-05-10 20:37:46 +0800

    editmenu/managerpage/menupage changes

--- entity/EditMenu.js
+++ entity/EditMenu.js
@@ -18,22 +18,22 @@ router.post('/', function(req, res){
   var itemPrice = req.body.itemPrice;
   var itemID = req.body.itemID;
   var db = new sqlite3.Database('restaurant.db');
-  console.log('Editing ' + itemType + ' menu item with old item ID: ' + itemID);
+  console.log('Editing ' + itemType + ' menu item with item ID: ' + itemID);
 
   if(itemType == 'food'){
-    db.run('UPDATE foodmenu SET item_name = ?, item_price = ? WHERE item_id = ?;',[itemName, itemPrice, itemID], function(err){
+    db.run('UPDATE foodmenu SET item_name = COALESCE(item_name, ?), item_price = COALESCE(item_price, ?) WHERE item_id = ?;',[itemName, itemPrice, itemID], function(err){
         if(err){
           alert("Error editing food menu item")
           console.log(err);
         }
-        else{
+        else{node
           alert("Food menu item successfully edited");
           console.log("Food menu item successfully edited");
         }
     });
   } 
   else if(itemType == 'drink'){
-    db.run('UPDATE drinkmenu SET item_name = ?, item_price = ? WHERE item_id = ?;',[itemName, itemPrice, itemID], function(err){
+    db.run('UPDATE drinkmenu SET item_name = COALESCE(item_name, ?), item_price = COALESCE(item_price, ?) WHERE item_id = ?;',[itemName, itemPrice, itemID], function(err){
       if(err){
         alert("Error editing drink menu item")
         console.log(err);
