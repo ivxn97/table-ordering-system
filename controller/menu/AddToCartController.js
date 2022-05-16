67b4f00c6e0c5b84dd2c7603796e232720f3ffc2
Ivan Lee <kudoboi@hotmail.com>
2022-05-16 22:30:30 +0800

    moved to subfolders

---
+++ controller/menu/AddToCartController.js
@@ -0,0 +1,15 @@
+// insert http://localhost:3000 into browser address bar
+var sqlite3 = require('sqlite3').verbose();
+var express = require('express');
+var path = require("path");
+var bodyParser = require ('body-parser');
+var router = express.Router();
+
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.json());
+router.use(express.static(path.join(__dirname + '../public')));
+router.use('/img', express.static(__dirname + '../Images'));
+
+var AddToCartController = require('../../entity/AddToCart');
+router.use('/', AddToCartController);
+module.exports = router;
