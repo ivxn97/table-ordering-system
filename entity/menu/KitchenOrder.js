baf52642acffd1bd9446e989e1a7c9230feb1911
mattyjy <104262448+mattyjy@users.noreply.github.com>
2022-05-24 17:30:58 +0800

    as

--- entity/menu/KitchenOrder.js
+++ entity/menu/KitchenOrder.js
@@ -25,7 +25,7 @@ let year = dateFormat.getFullYear();
 router.post('/', function(req, res){
 var email = req.body.email;
 var tableNo = req.body.tableNo;
-var dateFinal = (year + "-" + month + "-" + day);
+var dateFinal = (year + "-" + month + "-" + date);
 
 var db = new sqlite3.Database('restaurant.db');
 console.log('Sending Cart Order to Kitchen Staff');
